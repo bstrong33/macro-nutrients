9dfb31eed5e5fc9d21f27db2e35f60bb0b74f86e
bstrong33 <boston.strong33@gmail.com>
2019-10-01 22:59:44 -0600

    fixed index rendering bug

--- macro-nutrients/src/App.js
+++ macro-nutrients/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      {routes}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {routes}
+      </div>
+    );
+  }
 }
 
 export default App;
--- macro-nutrients/src/index.js
+++ macro-nutrients/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 import store from './ducks/store';
 
 ReactDOM.render(
@@ -13,10 +12,8 @@ ReactDOM.render(
             <App />
         </HashRouter>
     </Provider>
-
-, document.getElementById('root'));
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
