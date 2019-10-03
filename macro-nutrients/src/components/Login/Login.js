be52304e6b909600e13accedcd3f7ac62a22c585
bstrong33 <boston.strong33@gmail.com>
2019-10-02 20:08:19 -0600

    started to create login

--- macro-nutrients/src/components/Login/Login.js
+++ macro-nutrients/src/components/Login/Login.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Login extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            username: '',
+            password: '',
+            message: '',
+            toggleLogin: true
+        }
+
+    }
+
+    toggleLogin = () => {
+        this.setState({ toggleLogin: !this.state.toggleLogin})
     }
 
     render() {
         return (
-            <div>
+            <div className='login-background'>
+                {this.state.toggleLogin ?
+                <div className='login'>
+                    <div className='username'>
+                        <p>Username:
+                            <input
+                                value={this.state.username} onChange={e => this.setState({ username: e.target.value })}
+                            />
+                        </p>
+                    </div>
+                    <div className='password'>
+                        <p>Password:
+                            <input
+                                value={this.state.password} onChange={e => this.setState({ password: e.target.value })}
+                                type="password"
+                            />
+                        </p>
+                    </div>
+                    <button
+                        onClick={this.toggleLogin}
+                    >Register for Account</button>
+                </div>
+                :
+                <div className='register'>
 
+                </div>
+                }
             </div>
         )
     }
